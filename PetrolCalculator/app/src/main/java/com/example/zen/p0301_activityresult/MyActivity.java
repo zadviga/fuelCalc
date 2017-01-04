be120e4c0e8863ee557f4fef37af207a022236e3
Zadviga <edrobot@ukr.net>
2017-01-05 00:25:18 +0200

    no message

--- PetrolCalculator/app/src/main/java/com/example/zen/p0301_activityresult/MyActivity.java
+++ PetrolCalculator/app/src/main/java/com/example/zen/p0301_activityresult/MyActivity.java
@@ -40,8 +40,6 @@ public class MyActivity extends Activity implements View.OnClickListener {
             @Override
             public void onClick(View view) {
                 Toast.makeText(MyActivity.this, "Drive Safely", Toast.LENGTH_SHORT).show();
-          //      Intent intent = new Intent(this, AboutActivity.class);
-           //     startActivity(intent);
 
             }
         });
@@ -50,16 +48,12 @@ public class MyActivity extends Activity implements View.OnClickListener {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.my, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
             return true;
